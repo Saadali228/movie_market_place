1b20c81a7bac14f86b256c9b9fb49a13113e5bb8
saadsardar <k180228@nu.edu.pk>
2021-12-23 23:55:50 +0500

    added add to cart

--- lib/home_page/widgets/movie_grid.dart
+++ lib/home_page/widgets/movie_grid.dart
@@ -1,8 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart'
     as a;
+import 'package:movie_market_place/cart/bloc/cart_bloc.dart';
+import 'package:movie_market_place/cart/repository_layer/models/cart_repository_model.dart';
 import 'package:movie_market_place/detail_page/pages/detail_page.dart';
 import 'package:movie_market_place/home_page/bloc/movie_bloc.dart';
 import 'package:movie_market_place/home_page/repository_layer/models/movie_repo_model.dart';
@@ -29,51 +33,96 @@ class _MovieGridState extends State<MovieGrid> {
   Widget build(BuildContext context) {
     return BlocBuilder<MovieBloc, MovieState>(
       builder: (context, state) {
-        return Container(
-          color: const Color(0xff1F0C3F),
-          child: Center(
-            child: SingleChildScrollView(
-              controller: _scrollController,
-              child: AnimationLimiter(
-                child: Padding(
-                  padding: const EdgeInsets.only(top:40.0),
-                  child: Wrap(
-                    children: List.generate(
-                      state.movieList.length,
-                      (index) => AnimationConfiguration.staggeredList(
-                        position: index,
-                        duration: const Duration(milliseconds: 300),
-                        child: SlideAnimation(
-                          child: a.FadeInAnimation(
-                            child: Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Stack(
-                                children: [
-                                  Image.network(
-                                    "https://image.tmdb.org/t/p/w500/" +
-                                        state.movieList[index].poster!,
-                                    width: 250.0,
-                                  ),
-                                  Positioned(
-                                    right: 0,
-                                    top: 0,
-                                    child: ElevatedButton.icon(
-                                      onPressed: () {},
-                                      icon: const Icon(Icons.add),
-                                      label: const Text("BUY NOW"),
-                                      style: ButtonStyle(
-                                        shape: MaterialStateProperty.all<
-                                            RoundedRectangleBorder>(
-                                          const RoundedRectangleBorder(
-                                            borderRadius: BorderRadius.only(
-                                              bottomLeft: Radius.circular(8.0),
+        return BlocListener<CartBloc, CartState>(
+          listenWhen: (previous, current) =>
+              previous.addToCartStatus != current.addToCartStatus,
+          listener: (context, state) {
+            if (state.addToCartStatus == AddToCartStatus.loaded) {
+              Scaffold.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Item Added'),
+                  duration: Duration(milliseconds: 300),
+                ),
+              );
+              context.read<CartBloc>().add(AddCartInitial());
+            }
+            if (state.addToCartStatus == AddToCartStatus.error) {
+              Scaffold.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Item Not Added'),
+                  duration: Duration(milliseconds: 300),
+                ),
+              );
+              context.read<CartBloc>().add(AddCartInitial());
+            }
+          },
+          child: Container(
+            color: const Color(0xff1F0C3F),
+            child: Center(
+              child: SingleChildScrollView(
+                controller: _scrollController,
+                child: AnimationLimiter(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 40.0),
+                    child: Wrap(
+                      children: List.generate(
+                        state.movieList.length,
+                        (index) => AnimationConfiguration.staggeredList(
+                          position: index,
+                          duration: const Duration(milliseconds: 300),
+                          child: SlideAnimation(
+                            child: a.FadeInAnimation(
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Stack(
+                                  children: [
+                                    Image.network(
+                                      "https://image.tmdb.org/t/p/w500/" +
+                                          state.movieList[index].poster!,
+                                      width: 250.0,
+                                    ),
+                                    Positioned(
+                                      right: 0,
+                                      top: 0,
+                                      child: ElevatedButton.icon(
+                                        onPressed: () {
+                                          context.read<CartBloc>().add(
+                                                AddProduct(
+                                                  CartRepoModel(
+                                                    id: state
+                                                        .movieList[index].id,
+                                                    title: state
+                                                        .movieList[index]
+                                                        .title!,
+                                                    price: 50 +
+                                                        Random().nextInt(100),
+                                                    image: state
+                                                        .movieList[index]
+                                                        .poster!,
+                                                    // qty: movieList[index].id,
+                                                    // totalPrice: 1 * movieList[index].id +
+                                                    //     Random().nextDouble(),
+                                                  ),
+                                                ),
+                                              );
+                                        },
+                                        icon: const Icon(Icons.add),
+                                        label: const Text("BUY NOW"),
+                                        style: ButtonStyle(
+                                          shape: MaterialStateProperty.all<
+                                              RoundedRectangleBorder>(
+                                            const RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.only(
+                                                bottomLeft:
+                                                    Radius.circular(8.0),
+                                              ),
                                             ),
                                           ),
                                         ),
                                       ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
                             ),
                           ),
