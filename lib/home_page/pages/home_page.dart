f34efdd212ba19f7ed839edc5f265ceb3c0ceda9
khizrvd <khizr.farooqui@venturedive.com>
2021-12-30 15:31:00 +0500

    Movie Grid Fix

--- lib/home_page/pages/home_page.dart
+++ lib/home_page/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movie_market_place/cart/bloc/cart_bloc.dart';
 import 'package:movie_market_place/cart/pages/cart_page.dart';
+import 'package:movie_market_place/home_page/bloc/movie_bloc.dart';
 import 'package:movie_market_place/home_page/widgets/cart_button.dart';
 import 'package:movie_market_place/home_page/widgets/home_banner.dart';
 import 'package:movie_market_place/home_page/widgets/logo_widget.dart';
@@ -40,51 +41,56 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
-  Widget build(BuildContext context)  {
+  Widget build(BuildContext context) {
     SizeConfig().init(context);
-    return Scaffold(
-      backgroundColor: const Color(0xff1F0C3F),
-      appBar: AppBar(
-        title: Row(
-          children: [
+    return BlocProvider(
+      create: (context) => MovieBloc(
+        RepositoryProvider.of(context),
+      ),
+      child: Scaffold(
+        backgroundColor: const Color(0xff1F0C3F),
+        appBar: AppBar(
+          title: Row(
+            children: [
+              SizedBox(
+                width: 3 * SizeConfig.blockSizeHorizontal!,
+              ),
+              const LogoWidget(),
+            ],
+          ),
+          actions: [
+            Builder(builder: (context) {
+              return CartButton(
+                onTap: () {
+                  Scaffold.of(context).openEndDrawer();
+                },
+              );
+            }),
             SizedBox(
               width: 3 * SizeConfig.blockSizeHorizontal!,
             ),
-            const LogoWidget(),
+          ],
+          centerTitle: false,
+          automaticallyImplyLeading: false,
+          backgroundColor: controller.hasClients && controller.page == 1
+              ? const Color(0xff1F0C3F)
+              : Colors.transparent,
+          elevation: 0,
+        ),
+        endDrawer: const CartDrawer(),
+        extendBodyBehindAppBar: true,
+        body: PageView(
+          controller: controller,
+          scrollDirection: Axis.vertical,
+          children: [
+            HomeBanner(
+              controller: controller,
+            ),
+            MovieGrid(
+              pageController: controller,
+            ),
           ],
         ),
-        actions: [
-          Builder(builder: (context) {
-            return CartButton(
-              onTap: () {
-                Scaffold.of(context).openEndDrawer();
-              },
-            );
-          }),
-          SizedBox(
-            width: 3 * SizeConfig.blockSizeHorizontal!,
-          ),
-        ],
-        centerTitle: false,
-        automaticallyImplyLeading: false,
-        backgroundColor: controller.hasClients && controller.page == 1
-            ? const Color(0xff1F0C3F)
-            : Colors.transparent,
-        elevation: 0,
-      ),
-      endDrawer: const CartDrawer(),
-      extendBodyBehindAppBar: true,
-      body: PageView(
-        controller: controller,
-        scrollDirection: Axis.vertical,
-        children: [
-          HomeBanner(
-            controller: controller,
-          ),
-          MovieGrid(
-            pageController: controller,
-          ),
-        ],
       ),
     );
   }
