7f81d83592528cdc7a75bc0389d04247d9d7c430
saadsardar <k180228@nu.edu.pk>
2022-01-20 15:56:54 +0500

    added address page

--- lib/account/pages/address_book.dart
+++ lib/account/pages/address_book.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_market_place/account/widgets/account_grid.dart';
 import 'package:movie_market_place/home_page/pages/home_page.dart';
 import 'package:movie_market_place/home_page/widgets/logo_widget.dart';
 import 'package:movie_market_place/home_page/widgets/size_config.dart';
@@ -10,6 +11,30 @@ double _mobile = 700;
 class AddressBook extends StatelessWidget {
   static const addressBookPageRoute = '/address-book';
   const AddressBook({Key? key}) : super(key: key);
+  Widget buildColumn(msg1, msg2) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          msg1,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 17,
+          ),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        Text(
+          msg2,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 17,
+          ),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,6 +79,8 @@ class AddressBook extends StatelessWidget {
             horizontal: 60,
           ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const Text(
                 'Address Book',
@@ -63,8 +90,61 @@ class AddressBook extends StatelessWidget {
                   fontWeight: FontWeight.w500,
                 ),
               ),
-              Row(
-                children: [],
+              const SizedBox(height: 30.0),
+              Container(
+                height: size.height * 0.7,
+                color: const Color(0xff361F41),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 50.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          buildColumn(
+                            'Full Name',
+                            'Syed Saad Ali',
+                          ),
+                          // const SizedBox(
+                          //   width: 30,
+                          // ),
+                          buildColumn(
+                            'Address',
+                            'A300, Block N, North Nazimabad',
+                          ),
+                          buildColumn(
+                            'Region',
+                            'Sindh, Karachi, PK',
+                          ),
+                          buildColumn(
+                            'Phone Number',
+                            '03343586862',
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 50.0, top: 50.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ElevatedButton(
+                              onPressed: () {},
+                              child: const Text(
+                                '+Add New Address',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 17,
+                                ),
+                              ))
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ],
           ),
