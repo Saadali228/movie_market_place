eacfe3bade7304abf935652aa902c03b6ea80f85
khizrvd <khizr.farooqui@venturedive.com>
2022-01-21 11:03:35 +0500

    account in progress

--- lib/account/widgets/my_account.dart
+++ lib/account/widgets/my_account.dart
@@ -6,30 +6,30 @@ class MyAccount extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        const Text(
-          'Manage My Account',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
+    double mWidth = MediaQuery.of(context).size.width;
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Text(
+            'Manage My Account',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-        const SizedBox(height: 20.0),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
+          const SizedBox(height: 20.0),
+          Wrap(
+            spacing: 20,
+            direction: Axis.horizontal,
             children: const [
               AccountGrid(
                 heading: 'Personal Profile',
                 title: 'Vendian',
                 subTitle: 'venidan@venturedive.com',
               ),
-              SizedBox(width: 20),
               AccountGrid(
                 heading: 'Address Book',
                 title: 'Vendian',
@@ -38,88 +38,88 @@ class MyAccount extends StatelessWidget {
               ),
             ],
           ),
-        ),
-        const SizedBox(height: 20.0),
-        SizedBox(
-          height: 350,
-          width: 1000,
-          child: Card(
-            color: const Color(0xff361F41),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Recent Orders',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
+          const SizedBox(height: 20.0),
+          SizedBox(
+            height: 350,
+            width: mWidth * 0.65,
+            child: Card(
+              color: const Color(0xff361F41),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Recent Orders',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 20.0),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      textColumn(
-                        'Order #',
-                        '1',
-                        '2',
-                      ),
-                      textColumn(
-                        'Placed On',
-                        '31/12/2021',
-                        '19/11/2021',
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              'Items',
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 15,
-                            ),
-                            Image.network(
-                              'https://image.tmdb.org/t/p/w500/c6H7Z4u73ir3cIoCteuhJh7UCAR.jpg',
-                              scale: 10,
-                            ),
-                            const SizedBox(
-                              height: 15,
-                            ),
-                            Image.network(
-                              'https://image.tmdb.org/t/p/w500/1Rr5SrvHxMXHu5RjKpaMba8VTzi.jpg',
-                              scale: 10,
-                            ),
-                          ],
+                    const SizedBox(height: 20.0),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        textColumn(
+                          'Order #',
+                          '1',
+                          '2',
                         ),
-                      ),
-                      textColumn(
-                        'Total',
-                        '\$73',
-                        '\$84',
-                      ),
-                    ],
-                  ),
-                ],
+                        textColumn(
+                          'Placed On',
+                          '31/12/2021',
+                          '19/11/2021',
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Items',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              Image.network(
+                                'https://image.tmdb.org/t/p/w500/c6H7Z4u73ir3cIoCteuhJh7UCAR.jpg',
+                                scale: 10,
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              Image.network(
+                                'https://image.tmdb.org/t/p/w500/1Rr5SrvHxMXHu5RjKpaMba8VTzi.jpg',
+                                scale: 10,
+                              ),
+                            ],
+                          ),
+                        ),
+                        textColumn(
+                          'Total',
+                          '\$73',
+                          '\$84',
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
