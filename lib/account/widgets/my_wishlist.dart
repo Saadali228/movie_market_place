1e980624adfaacd53b1f9af7e3f19246ac26907c
khizrvd <khizr.farooqui@venturedive.com>
2022-01-21 15:52:32 +0500

    account in progress

---
+++ lib/account/widgets/my_wishlist.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MyWishlist extends StatelessWidget {
+  const MyWishlist({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: const [
+          Text(
+            'My Wishlist',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
